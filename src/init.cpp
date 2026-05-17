b9ff04ed3cadedfaa851f4b9eafe90ee539b3d30
Percash23 <bpercle2323@gmail.com>
2026-05-17 10:58:41 -0500

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -1,1 +1,24 @@
+bool AppInit(int argc, char* argv[], node::NodeContext& node)
+{
+    SetupNetworking();
 
+    if (!AppInitBasicSetup())
+        return false;
+
+    if (!AppInitParameterInteraction())
+        return false;
+
+    if (!AppInitSanityChecks())
+        return false;
+
+    if (!AppInitLockDataDirectory())
+        return false;
+
+    if (!AppInitInterfaces(node))
+        return false;
+
+    if (!AppInitMain(node))
+        return false;
+
+    return true;
+}
